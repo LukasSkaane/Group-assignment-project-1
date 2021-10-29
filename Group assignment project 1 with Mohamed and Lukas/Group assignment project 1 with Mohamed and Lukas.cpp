c1f8a91bf9fa94aba1f22065fe5f37fe2d95656b
Lukas Skaane <270139160@yoobeestudent.ac.nz>
2021-10-29 03:32:32 +0200

    Created project

---
+++ Group assignment project 1 with Mohamed and Lukas/Group assignment project 1 with Mohamed and Lukas.cpp
@@ -0,0 +1,11 @@
+// Group assignment project 1 with Mohamed and Lukas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "Hello world" << endl;
+}
